faa9d2b18143efbd310eced2f75f7d0869fb693d
Maxwell Beck <max@rastertail.net>
2023-03-04 12:47:55 -0600

    Initialize Nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rust = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ rust ];
+        };
+      }
+    );
+}
