8654c465ee769a4c159e09cf7849a26f4b334bee
Maxwell Beck <max@rastertail.net>
2023-03-07 17:03:40 -0600

    Fix dev_serve.sh termination

--- dev_serve.sh
+++ dev_serve.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 simple-http-server --index web &
-cargo watch -i .gitignore -i "web/*" -s "wasm-pack build --target web -d web/dist"
+cargo watch -i .gitignore -i "web/*" -s "wasm-pack build --target web -d web/dist" &&
+kill $!
